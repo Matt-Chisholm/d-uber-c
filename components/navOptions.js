63769eb231ecafa36a1f5e0ae15a42a015215e7f
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-11-30 14:32:48 -0800

    flatlist img and icon

--- components/navOptions.js
+++ components/navOptions.js
@@ -1,5 +1,7 @@
 import { Text, View, FlatList, Image, TouchableOpacity } from "react-native";
 import React from "react";
+import tw from "tailwind-react-native-classnames";
+import { Icon } from "react-native-elements";
 
 const data = [
 	{
@@ -21,10 +23,23 @@ const NavOptions = () => {
 		<View>
 			<FlatList
 				data={data}
+				horizontal
 				keyExtractor={(item) => item.id}
 				renderItem={({ item }) => (
-					<TouchableOpacity>
-						<Text>{item.title}</Text>
+					<TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
+						<View>
+							<Image
+								style={{ width: 120, height: 120, resizeMode: "contain" }}
+								source={{ uri: item.image }}
+							/>
+							<Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
+							<Icon
+								style={tw`p-2 bg-black rounded-full w-10 mt-4`}
+								name='arrowright'
+								color='white'
+								type='antdesign'
+							/>
+						</View>
 					</TouchableOpacity>
 				)}
 			/>

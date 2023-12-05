fafae10a1e981fd64b6c97d5d996fd293277738e
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-12-05 13:24:17 -0800

    api debugging

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,10 +2,11 @@ import React from "react";
 import { View, SafeAreaView, StyleSheet, Image } from "react-native";
 import tw from "tailwind-react-native-classnames";
 import NavOptions from "../components/navOptions";
-import { GooglePlacesAutoComplete } from "react-native-google-places-autocomplete";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 import { GOOGLE_MAPS_API_KEY } from "@env";
 
 const HomeScreen = () => {
+	console.log(GooglePlacesAutoComplete);
 	return (
 		<SafeAreaView style={tw`bg-white h-full`}>
 			<View style={tw`p-5`}>
@@ -19,10 +20,17 @@ const HomeScreen = () => {
 						uri: "https://links.papareact.com/gzs",
 					}}
 				/>
-				<GooglePlacesAutoComplete
-					placeholder='Where From?'
-					nearbyPlacesAPI='GooglePlacesSearch'
-					debounce={400}
+				<GooglePlacesAutocomplete
+					placeholder='Search'
+					onPress={(data, details = null) => {
+						// 'details' is provided when fetchDetails = true
+						console.log(data, details);
+					}}
+					onFail={(error) => console.error(error)}
+					query={{
+						key: GOOGLE_MAPS_API_KEY,
+						language: "en",
+					}}
 				/>
 				<NavOptions />
 			</View>

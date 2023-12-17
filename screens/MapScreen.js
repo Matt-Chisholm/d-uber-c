a35df62817ac04cf68b0e3fe5ee4ec530dc821ff
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-12-17 10:03:53 -0800

    mapview working

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,10 +1,19 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import Mapview from "react-native-maps";
 
 const MapScreen = () => {
 	return (
 		<View>
-			<Text>MapScreen</Text>
+			<Mapview
+				style={{ height: "100%", width: "100%" }}
+				initialRegion={{
+					latitude: 28.450627,
+					longitude: -16.263045,
+					latitudeDelta: 0.0922,
+					longitudeDelta: 0.0421,
+				}}
+			/>
 		</View>
 	);
 };

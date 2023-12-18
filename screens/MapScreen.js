40de3ce6822f4e02f96e625db70f51ea04c261ab
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-12-18 09:26:54 -0800

    marker working

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,20 +1,34 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
-import Mapview from "react-native-maps";
+import Mapview, { Marker } from "react-native-maps";
 import tw from "tailwind-react-native-classnames";
+import { useSelector } from "react-redux";
 
 const MapScreen = () => {
+	const origin = useSelector((state) => state.nav.origin);
+
 	return (
 		<Mapview
 			style={{ height: "50%", width: "100%" }}
 			mapType='mutedStandard'
 			initialRegion={{
-				latitude: 28.450627,
-				longitude: -16.263045,
-				latitudeDelta: 0.0922,
-				longitudeDelta: 0.0421,
-			}}
-		/>
+				latitude: origin.location.lat,
+				longitude: origin.location.lng,
+				latitudeDelta: 0.005,
+				longitudeDelta: 0.005,
+			}}>
+			{origin?.location && (
+				<Marker
+					coordinate={{
+						latitude: origin.location.lat,
+						longitude: origin.location.lng,
+					}}
+					title='Origin'
+					description={origin.description}
+					identifier='origin'
+				/>
+			)}
+		</Mapview>
 	);
 };
 
